02035ced799b52f9e9419b29dfba75f1b938cb2b
Etchegoyen Matthieu <etchegom@gmail.com>
2024-08-21 12:59:17 +0200

    WIP

--- recoco/apps/resources/tests/test_models.py
+++ recoco/apps/resources/tests/test_models.py
@@ -1,3 +1,10 @@
-# FIXME: This test is not complete. It is just a placeholder for now.
+import pytest
+from model_bakery import baker
+
+
+@pytest.mark.django_db
 def test_is_dsresource():
-    pass
+    resource = baker.make("resources.Resource")
+    assert resource.is_dsresource is False
+    baker.make("demarches_simplifiees.DSResource", resource=resource)
+    assert resource.is_dsresource is True
