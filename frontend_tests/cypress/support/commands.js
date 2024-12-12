ad112061ad3f0ed789d4e36c56a111d19e0cacbd
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2024-12-12 09:13:29 +0100

    [test] deactivate captcha in settings

--- frontend_tests/cypress/support/commands.js
+++ frontend_tests/cypress/support/commands.js
@@ -405,13 +405,3 @@ Cypress.Commands.add(
     });
   }
 );
-
-Cypress.Commands.add('clickRecaptcha', () => {
-  cy.window().then((win) => {
-    win.document
-      .querySelector("iframe[src*='recaptcha']")
-      .contentDocument.getElementById('recaptcha-token')
-      .click();
-    cy.wait(500);
-  });
-});
